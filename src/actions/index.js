75933fe57e9f546e4324526f24005106e907a998
Michael Terry <kanon4us@yahoo.com>
2016-08-18 23:27:43 -0500

    Trying Old URL
    http://jsonplaceholder.typicode.com/users from the class in action/index

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 import { FETCH_USERS } from './types';
 
 export function fetchUsers() {
-	const request = axios.get('https://www.beermenus.com/menu_widgets/26209.json');
+	const request = axios.get('http://jsonplaceholder.typicode.com/users');
 
-	instance.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+	//instance.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 
 	return {
 		type: FETCH_USERS,
